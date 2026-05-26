f0d770d219c0d71468acff9d5ffedc05ca7fc5a8
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 04:38:37 +0900

    Delete utils/firestore.js

--- utils/firestore.js
+++
@@ -1,8 +0,0 @@
-// /admin/utils/firestore.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-export const app = initializeApp({
-  apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
-  authDomain: "eternal-fantasy.firebaseapp.com",
-  projectId: "eternal-fantasy"
-});
