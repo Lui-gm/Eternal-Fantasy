e7525002e8ab4cf1976597a1003b915c41db9234
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 07:38:50 +0900

    Update auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+  getAuth, 
+  onAuthStateChanged, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+  getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
@@ -8,9 +15,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-// 未ログインなら login.html に飛ばす
+// 未ログインなら /auth/login.html に飛ばす
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     location.href = "login.html";
