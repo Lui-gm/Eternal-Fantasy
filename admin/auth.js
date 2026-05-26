2aae7317ae935e0fdf0fc05fe608f6b84f045f1a
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 07:48:58 +0900

    Update auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -1,32 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// Firebase 初期化は firebase.js が行う
+import { auth, db } from "../js/firebase.js";
 import { 
-  getAuth, 
   onAuthStateChanged, 
   signOut 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { 
-  getFirestore 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
-  authDomain: "eternal-fantasy.firebaseapp.com",
-  projectId: "eternal-fantasy"
-};
-
-const app = initializeApp(firebaseConfig);
-
-// ★ これが無いと pages 側が全部死ぬ
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-// ★ login.html が admin フォルダ直下ならこれで正しい
+// 未ログインなら admin/login.html に飛ばす
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     location.href = "login.html";
   }
 });
 
+// ログアウト
 window.logout = () => {
   signOut(auth);
 };
