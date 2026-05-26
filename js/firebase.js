a225053333eb4e6711562d2443d055b39dee5eef
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 02:06:43 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,17 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
-// Firebase 設定（公式コードベース）
 const firebaseConfig = {
   apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
   authDomain: "eternal-fantasy.firebaseapp.com",
   projectId: "eternal-fantasy",
-  storageBucket: "eternal-fantasy.appspot.com", // ← ここだけ公式コードのままだと動かない
+  storageBucket: "eternal-fantasy.appspot.com",
   messagingSenderId: "547745935105",
   appId: "1:547745935105:web:69cc7119b9d5470e9c7500",
   measurementId: "G-1Q4CBRQFL3"
 };
 
-// Firebase 初期化
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
