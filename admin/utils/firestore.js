fb1b3a1b610a0e914d678cb766cbf054a3631a21
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 05:05:54 +0900

    Update firestore.js

--- admin/utils/firestore.js
+++ admin/utils/firestore.js
@@ -1,8 +1,13 @@
 // /admin/utils/firestore.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 export const app = initializeApp({
   apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
   authDomain: "eternal-fantasy.firebaseapp.com",
   projectId: "eternal-fantasy"
 });
+
+// Firestore インスタンスを作成して export
+export const db = getFirestore(app);
