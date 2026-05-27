e1e1fe067f7c47aa6ab857acc218c2aab43271b0
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 21:20:09 +0900

    Update debug.js

--- admin/pages/debug.js
+++ admin/pages/debug.js
@@ -1,1 +1,16 @@
+export const html = `
+  <h2>デバッグ</h2>
+  <p>開発用のデバッグツールです。</p>
 
+  <button id="testBtn">テスト実行</button>
+
+  <pre id="debugOutput"></pre>
+`;
+
+export function loadPage(main) {
+  main.innerHTML = html;
+
+  document.getElementById("testBtn").addEventListener("click", () => {
+    document.getElementById("debugOutput").innerText = "（デバッグ機能は未実装）";
+  });
+}
