cf67381a4f117b13d892d1244e69c9416b7d8e60
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 22:14:28 +0900

    Update maintenance.js

--- admin/pages/maintenance.js
+++ admin/pages/maintenance.js
@@ -1,5 +1,5 @@
 import { db } from "../../js/firebase.js";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 export function loadPage(main) {
   main.innerHTML = `
