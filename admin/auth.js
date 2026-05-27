8c069135b189da3a0ab490faed9fa16f010a990c
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 20:18:10 +0900

    Update auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -4,7 +4,7 @@
 
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { auth, db } from "./firebase.js";
+import { auth, db } from "../firebase.js";
 
 export async function login(email, password) {
   try {
