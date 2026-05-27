de83c320a8608e0c1df7de833533996eedbfc1a6
Lui-gm <58cxx4x4xk@privaterelay.appleid.com>
2026-05-27 23:18:29 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,8 @@
-// Firebase CDN 版（GitHub Pages 対応）
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDzA2CXQQTR4cV0if4ahfOJx98y5k6Rhp4",
   authDomain: "eternal-fantasy.firebaseapp.com",
   projectId: "eternal-fantasy",
@@ -13,3 +13,4 @@ export const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
